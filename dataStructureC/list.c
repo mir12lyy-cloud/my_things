60d93ce0c5b75bf0ffbcfbfe2afbcc9875cfede6
casyyy <huangjinyangyang@hotmail.com>
2026-03-08 22:34:26 +0800

    Add things

--- dataStructureC/list.c
+++ dataStructureC/list.c
@@ -1,4 +1,11 @@
 #include<stdlib.h>
 
-#define List(type)\
+#define List(type) \
+typedef struct type##_list{ \
+    type data; \
+    struct type##_list *prev; \
+    struct type##_list *next; \
+}type##_list; \
+type##_list *head = NULL, *tail = NULL; \
+
 
