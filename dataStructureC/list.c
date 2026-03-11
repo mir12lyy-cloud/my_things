845c527db568f712d886f6643f1c2a6969cd60ce
casyyy <mir12lyy@gmail.com>
2026-03-11 21:45:54 +0800

    modified:   dataStructureC/list.c 	modified:   dataStructureC/vectors.c

--- dataStructureC/list.c
+++ dataStructureC/list.c
@@ -14,12 +14,170 @@ type##_list* creatList(){ \
     bList -> next = NULL; bList -> prev = NULL; \
     return bList; \
 }\
-void push_back(type##_list* lList, type value){ \
+void destroy_list(type##_list** bList){\
+    if(bList == NULL || bList == NULL || *bList -> next == NULL) return; \
+    type##_list* begin = *bList; \  
+    type##_list* nList = NULL; \
+    while(begin != NULL){\
+        nList = begin -> next;\
+        free(begin);\
+        begin = nList; \
+    } \
+    *bList = NULL; \
+}\
+type##_list* push_back(type##_list** lList, type value){ \
+    assert(bList != NULL && *bList != NULL); \
     type##_list* eList = (type##_list*)malloc(sizeof(type##_list)); \
     assert(eList != NULL); \
     eList -> data = value; \
-    eList -> prev = lList; \
+    eList -> prev = *lList; \
     eList -> next = NULL; \
+    return eList; \
 } \ 
+void push_front(type##_list** bList, type value){ \
+    assert(bList != NULL && *bList != NULL); \
+    type##_list* nList = (type##_list*)malloc(sizeof(type##_list)); \
+    assert(nList != NULL); \
+    nList -> data = value; \
+    type##_list* fList = bList -> next; \
+    fList -> prev = nList; \
+    nList -> prev = *bList; \
+    nList -> next = fList; \
+    *bList -> next = nList; \
+}\
+void pop_front(type##_list** bList){\
+    assert(bList != NULL && *bList == NULL); \
+    if(*bList -> next = NULL) return; \
+    type##_list* eList = *bList -> next;\
+    type##_list* nList = *eList -> next;\
+    *bList -> next = nList; \
+    nList -> prev = *bList; \
+    free(eList); \
+    eList = NULL; \
+}\
+void pop_back(type##_list** eList){\
+    assert(eList != NULL && *eList == NULL); \
+    if(*eList -> prev = NULL) return; \
+    type##_list* nList = *eList -> prev;\
+    nList -> next = NULL; \
+    free(*eList); \
+    *eList = nList; \
+}\
+type##_list* get_value(type##_list* bList, size_t index){ \
+    assert(bList != NULL);\
+    type##_list* resPtr = NULL;\
+    type##_list* nList = bList;\
+    while(index--){\
+        resPtr = nList -> next;\
+        nList = resPtr;\
+    } \
+    return resPtr;\
+} \
+type##_list* rget_value(type##_list* rbList, size_t index){ \
+    assert(rbList != NULL);\
+    type##_list* resPtr = NULL;\
+    type##_list* nList = rbList;\
+    while(index--){\
+        resPtr = nList -> prev;\
+        nList = resPtr;\
+    } \
+    return resPtr;\
+} \
+size_t getSize(type##_list* bList){\
+    assert(bList != NULL);\
+    size_t size = 0;\
+    for(type##_list* begin = bList; begin -> next != NULL; begin = begin -> next, ++size); \
+    return size; \
+}\
 
 
+#define List_with_object(type) \
+typedef struct type##_list{ \
+    type data; \
+    struct type##_list *prev; \
+    struct type##_list *next; \
+}type##_list; \
+\
+type##_list* creatList(){ \
+    type##_list* bList = (type##_list*)malloc(sizeof(type##_list)); \
+    assert(bList != NULL); \
+    bList -> next = NULL; bList -> prev = NULL; \
+    return bList; \
+}\
+void destroy_list(type##_list** bList){\
+    if(bList == NULL || bList == NULL || *bList -> next == NULL) return; \
+    type##_list* begin = *bList; \  
+    type##_list* nList = NULL; \
+    while(begin != NULL){\
+        nList = begin -> next;\
+        free(begin -> data);\
+        free(begin);\
+        begin = nList; \
+    } \
+    *bList = NULL; \
+}\
+type##_list* push_back(type##_list** lList, type value){ \
+    assert(bList != NULL && *bList != NULL); \
+    type##_list* eList = (type##_list*)malloc(sizeof(type##_list)); \
+    assert(eList != NULL); \
+    eList -> data = value; \
+    eList -> prev = *lList; \
+    eList -> next = NULL; \
+    return eList; \
+} \ 
+void push_front(type##_list** bList, type value){ \
+    assert(bList != NULL && *bList != NULL); \
+    type##_list* nList = (type##_list*)malloc(sizeof(type##_list)); \
+    assert(nList != NULL); \
+    nList -> data = value; \
+    type##_list* fList = bList -> next; \
+    fList -> prev = nList; \
+    nList -> prev = *bList; \
+    nList -> next = fList; \
+    *bList -> next = nList; \
+}\
+void pop_front(type##_list** bList){\
+    assert(bList != NULL && *bList == NULL); \
+    if(*bList -> next = NULL) return; \
+    type##_list* eList = *bList -> next;\
+    type##_list* nList = *eList -> next;\
+    *bList -> next = nList; \
+    nList -> prev = *bList; \
+    free(eList -> data); \
+    free(eList); \
+    eList = NULL; \
+}\
+void pop_back(type##_list** eList){\
+    assert(eList != NULL && *eList == NULL); \
+    if(*eList -> prev = NULL) return; \
+    type##_list* nList = *eList -> prev;\
+    nList -> next = NULL; \
+    free(*eList); \
+    *eList = nList; \
+}\
+type##_list* get_value(type##_list* bList, size_t index){ \
+    assert(bList != NULL);\
+    type##_list* resPtr = NULL;\
+    type##_list* nList = bList;\
+    while(index--){\
+        resPtr = nList -> next;\
+        nList = resPtr;\
+    } \
+    return resPtr;\
+} \
+type##_list* rget_value(type##_list* rbList, size_t index){ \
+    assert(rbList != NULL);\
+    type##_list* resPtr = NULL;\
+    type##_list* nList = rbList;\
+    while(index--){\
+        resPtr = nList -> prev;\
+        nList = resPtr;\
+    } \
+    return resPtr;\
+} \
+size_t getSize(type##_list* bList){\
+    assert(bList != NULL);\
+    size_t size = 0;\
+    for(type##_list* begin = bList; begin -> next != NULL; begin = begin -> next, ++size); \
+    return size; \
+}\
--- dataStructureC/vectors.c
+++ dataStructureC/vectors.c
@@ -164,6 +164,168 @@ void pop_back(type##_vector* aVector){\
     --(aVector -> elem_end_);\
 }\
 
+#define Vector_with_object(type) \
+typedef struct{ \
+    type* data_; \
+    type* elem_end_; \
+    type* end_; \
+}object_##type##_vector; \
+object_##type##_vector creatVector(const size_t size){ \
+    size_t proportion_ = (size + NORMAL_SIZE - 1) / NORMAL_SIZE; \
+    size_t capacity = 0; \
+    if(!proportion_) capacity = NORMAL_SIZE; \
+    else capacity = proportion_ * NORMAL_SIZE; \
+    object_##type##_vector returnVector; \
+    returnVector.data_ = malloc(capacity * sizeof(type)); \
+    if(returnVector.data_ == NULL){ \
+        printf("Fail.\n"); \
+        returnVector.elem_end_ = NULL; \
+        returnVector.end_ = NULL; \
+        return returnVector; \
+    } \
+    returnVector.end_ = returnVector.data_ + capacity; \
+    returnVector.elem_end_ = returnVector.data_ + size; \
+    return returnVector; \
+} \
+void destroyVector(object_##type##_vector* aVector){ \
+    if(aVector == NULL){ \
+        printf("Illegal input.\n"); \
+        return; \
+    } \
+    if(aVector -> data_ == NULL){ \
+        printf("Fail.\n"); \
+        return; \
+    } \
+    for(type* it = data_; it != end_; ++it) free(*it); \
+    free(aVector -> data_); \
+    aVector -> elem_end_ = NULL; \
+    aVector -> end_ = NULL; \
+    aVector -> data_ = NULL; \
+} \
+type* begin(const object_##type##_vector* aVector) {return aVector -> data_;}\
+type* end(const object_##type##_vector* aVector) {return aVector -> elem_end_;}\
+size_t getSize(const object_##type##_vector* aVector) {return aVector -> elem_end_ - aVector -> data_;}\
+size_t getCapacity(const object_##type##_vector* aVector) {return aVector -> end_ - aVector -> data_;}\
+void reserve(object_##type##_vector* aVector, const size_t capacity){\
+    if(aVector == NULL){\
+        printf("Illegal input.\n");\
+        return;\
+    }\
+    if(aVector -> data_ == NULL){\
+        printf("Fail.\n");\
+        return;\
+    }\
+    size_t newsize = getSize(aVector);\
+    if(getCapacity(aVector) >= capacity) return;\
+    size_t alloc = ((capacity + NORMAL_SIZE - 1) / NORMAL_SIZE) * NORMAL_SIZE;\
+    type* newto = realloc(aVector -> data_, alloc * sizeof(type));\
+    if(newto == NULL){\
+        printf("Fail to reserve.\n");\
+        return;\
+    }\
+    aVector -> data_ = newto;\
+    aVector -> elem_end_ = aVector -> data_ + newsize;\
+    aVector -> end_ = aVector -> data_ + alloc;\
+}\
+void shrink(object_##type##_vector* aVector){\
+     if(aVector == NULL){\
+        printf("Illegal input.\n");\
+        return;\
+    }\
+    if(aVector -> data_ == NULL){\
+        printf("Fail.\n");\
+        return;\
+    }\
+    size_t newsize = getSize(aVector);\
+    type* newto = realloc(aVector -> data_, getSize(aVector) * sizeof(type));\
+    if(newto == NULL){\
+        printf("Fail to shrink.\n");\
+        return;\
+    }\
+    aVector -> data_ = newto;\
+    aVector -> elem_end_ = aVector -> data_ + newsize;\
+    aVector -> end_ = aVector -> data_ + newsize;\
+}\
+void expand(object_##type##_vector* aVector){\
+    if(aVector == NULL){\
+        printf("Illegal input.\n");\
+        return;\
+    }\
+    if(aVector -> data_ == NULL){\
+        printf("Fail.\n");\
+        return;\
+    }\
+    size_t newcapcity = 2 * getCapacity(aVector), newsize = getSize(aVector); \
+    type* newto = realloc(aVector -> data_, newcapcity * sizeof(type));\
+    if(newto == NULL){\
+        printf("Fail.\n");\
+        return;\
+    }\
+    aVector -> data_ = newto;\
+    aVector -> end_ = aVector -> data_ + newcapcity;\
+    aVector -> elem_end_ = aVector -> data_ + newsize;\
+}\
+void resize(object_##type##_vector* aVector, const size_t newsize){\
+    if(aVector == NULL){\
+        printf("Illegal input.\n");\
+        return;\
+    }\
+    if(aVector -> data_ == NULL){\
+        printf("Fail to resize\n");\
+        return;\
+    }\
+    if(newsize <= getCapacity(aVector)){\
+        aVector -> elem_end_ = aVector -> data_ + newsize;\
+        return;\
+    }\
+    reserve(aVector, newsize);\
+    aVector -> elem_end_ = aVector -> data_ + newsize;\
+}\
+void clear(object_##type##_vector* aVector){\
+    if(aVector == NULL){\
+        printf("Illegal input.\n");\
+        return;\
+    }\
+    if(aVector -> data_ == NULL){\
+        printf("Fail to clear\n");\
+        return;\
+    }\
+    aVector -> elem_end_ = aVector -> data_;\
+}\
+void push_back(type##_vector* aVector, const type value){\
+    if(aVector == NULL){\
+        printf("Illegal input.\n");\
+        return;\
+    }\
+    if(aVector -> data_ == NULL){\
+        printf("Fail\n");\
+        return;\
+    }\
+    if(aVector -> end_ == aVector -> elem_end_) expand(aVector);\
+    if(aVector -> end_ == aVector -> elem_end_){\
+        printf("Fail to push\n");\
+        return;\
+    }\
+    *(aVector -> elem_end_) = value;\
+    ++(aVector -> elem_end_);\
+}\
+void pop_back(object_##type##_vector* aVector){\
+    if(aVector == NULL){\
+        printf("Illegal input.\n");\
+        return;\
+    }\
+    if(aVector -> data_ == NULL){\
+        printf("Fail\n");\
+        return;\
+    }\
+    if(aVector -> elem_end_ == aVector -> data_){\
+        printf("Fail to pop\n");\
+        return;\
+    }\
+    --(aVector -> elem_end_);\
+    free(*(aVector -> elem_end_));\
+}\
+
 Vector(char)
 
 int main(){
